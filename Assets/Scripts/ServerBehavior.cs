885eece3a4e0e96c9da03b5243896831144eb717
Leslie Xie <thelxie27@gmail.com>
2024-04-04 21:14:43 -0400

    Base server code

--- Assets/Scripts/ServerBehavior.cs
+++ Assets/Scripts/ServerBehavior.cs
@@ -1,18 +1,82 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
-public class ServerBehavior : MonoBehaviour
+using Unity.Collections;
+using Unity.Networking.Transport;
+
+public class ServerBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public NetworkDriver m_Driver;
+    private NativeList<NetworkConnection> m_Connections;
+
     void Start()
     {
-        
+        m_Driver = NetworkDriver.Create();
+        var endpoint = NetworkEndpoint.AnyIpv4;
+        endpoint.Port = 9000;
+
+        if (m_Driver.Bind(endpoint) != 0)
+            Debug.Log("Failed to bind server driver to port" + endpoint.Port);
+        else
+            m_Driver.Listen();
+
+        m_Connections = new NativeList<NetworkConnection>(16, Allocator.Persistent);
     }
 
-    // Update is called once per frame
+    public void OnDestroy()
+    {
+        if (m_Driver.IsCreated)
+        {
+            m_Driver.Dispose();
+            m_Connections.Dispose();
+        }
+    }
+
     void Update()
     {
-        
+        m_Driver.ScheduleUpdate().Complete();
+        // Clean up connections
+        for (int i = 0; i < m_Connections.Length; i++)
+        {
+            if (!m_Connections[i].IsCreated)
+            {
+                m_Connections.RemoveAtSwapBack(i);
+                --i;
+            }
+        }
+        // Accept new connections
+        NetworkConnection c;
+        while ((c = m_Driver.Accept()) != default(NetworkConnection))
+        {
+            m_Connections.Add(c);
+            Debug.Log("Accepted a connection");
+        }
+        DataStreamReader stream;
+        for (int i = 0; i < m_Connections.Length; i++)
+        {
+            if (!m_Connections[i].IsCreated)
+                continue;
+
+            NetworkEvent.Type cmd;
+            while ((cmd = m_Driver.PopEventForConnection(m_Connections[i], out stream)) != NetworkEvent.Type.Empty)
+            {
+                if (cmd == NetworkEvent.Type.Data)
+                {
+                    uint number = stream.ReadUInt();
+                    Debug.Log("Got " + number + " from the Client adding + 2 to it.");
+                    number += 2;
+
+                    m_Driver.BeginSend(NetworkPipeline.Null, m_Connections[i], out var writer);
+                    writer.WriteUInt(number);
+                    m_Driver.EndSend(writer);
+                }
+                else if (cmd == NetworkEvent.Type.Disconnect)
+                {
+                    Debug.Log("Client disconnected from server");
+                    m_Connections[i] = default(NetworkConnection);
+                }
+            }
+        }
     }
 }
